d5ce382ececb014b4ea3d1a13bfcb233129936a9
settc <tewhittle@gmail.com>
2023-07-07 13:26:54 -0400

    Add components with 'Header' test component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Header from "./components/Header/Header.js"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+        <Header />
     </div>
   );
 }
