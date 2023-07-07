c90d156487b37da58dc41aedbcc8b8782234a245
settc <tewhittle@gmail.com>
2023-07-07 14:41:02 -0400

    Add a container to hold main page content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header/Header.js"
+import Container from "./components/Container/Container.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Main from "./pages/main.js";
 import Work from "./pages/work.js";
@@ -6,17 +7,20 @@ import Play from "./pages/play.js";
 import Contact from "./pages/contact.js";
 
 import './App.css';
+import Navbar from "./components/Navbar/Navbar.js";
 
 function App() {
   return (
     <div className="App">
-      <Router>        
-        <Routes>
-          <Route exact path="/" element={<Main />} />
-          <Route path='/work' element={<Work />} />            
-          <Route path='/play' element={<Play />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
+      <Router>
+        <Container>
+          <Routes>
+            <Route exact path="/" element={<Main />} />
+            <Route path='/work' element={<Work />} />            
+            <Route path='/play' element={<Play />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </Container>              
       </Router>  
     </div>
   );
