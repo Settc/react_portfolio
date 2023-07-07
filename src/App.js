618929c379c40cfae463460ad61ed3d6543e5a9c
settc <tewhittle@gmail.com>
2023-07-07 14:06:17 -0400

    Add pages and their corresponding routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Header from "./components/Header/Header.js"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Main from "./pages/main.js";
+import Work from "./pages/work.js";
+import Play from "./pages/play.js";
+import Contact from "./pages/contact.js";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Header />
+      <Router>        
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route path='/work' element={<Work />} />            
+          <Route path='/play' element={<Play />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router>  
     </div>
   );
 }
